e6c4458e0c62ebf7c510dbc5a8ffb349c1c7d5fd
Phil Wells <phil.wells@kaplan.com>
2017-12-06 09:22:08 -0500

    add, subtract, scalarMultiply

--- vector.js
+++ vector.js
@@ -9,12 +9,51 @@ class Vector {
                 return element === otherVector.coordinates[index];
             })));
         }
+
+        this.add = (otherVector) => {
+            const sum = [];
+            let index = 0;
+            this.coordinates.forEach((coordinate) => {
+                sum.push(coordinate + otherVector.coordinates[index]);
+                index++;
+            });
+            return new Vector(sum);
+        }
+
+        this.subtract = (otherVector) => {
+            const diff = [];
+            let index = 0;
+            this.coordinates.forEach((coordinate) => {
+                diff.push(coordinate - otherVector.coordinates[index]);
+                index++;
+            });
+            return new Vector(diff);
+        }
+
+        this.scalarMultiply = (multiplier) => {
+            const product = [];
+            this.coordinates.forEach((coordinate) => {
+                product.push(coordinate * multiplier);
+            });
+            return new Vector(product);
+        }
     }
 }
 
-let vector1 = new Vector([1, 2, 3]);
-let vector2 = new Vector([1,2,3]);
-let vector3 = new Vector([-1,2,3]);
-vector1.print();
-console.log(vector1.equals(vector2));
-console.log(vector1.equals(vector3));
+let vector1 = new Vector([8.218, -9.341]);
+let vector2 = new Vector([-1.129, 2.111]);
+
+let vector3 = new Vector([7.119, 8.215]);
+let vector4 = new Vector([-8.223, 0.878]);
+
+let vector5 = new Vector([1.671, -1.012, -0.318]);
+
+let vector6 = vector1.add(vector2);
+let vector7 = vector3.subtract(vector4);
+
+let vector8 = vector5.scalarMultiply(7.41);
+
+vector6.print();
+vector7.print();
+vector8.print();
+
